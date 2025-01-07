8a013e56263cf08e67badbec7e17d89d774ab610
KIMDOTS <mjmj27328125@gmail.com>
2025-01-07 14:27:53 +0900

    first-commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { MenuProvider } from './context/menuContext';
+import { CartProvider } from './context/cartContext.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <MenuProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </MenuProvider>
   </BrowserRouter>
-);
+)
